56284ec170299f0ebffe1e9875992d08a68423a9
현수빈 <sunny5875@uniwaffle.com>
2024-04-15 21:11:54 +0900

    [Fix]: fix some sentence

--- WidgetPractice/MyWidget/Widget/IntentWidget/IntentWidget.swift
+++ WidgetPractice/MyWidget/Widget/IntentWidget/IntentWidget.swift
@@ -20,8 +20,8 @@ struct IntentWidget: Widget {
             IntentWidgetEntryView(entry: entry)
                 .containerBackground(.clear, for: .widget)
         }
-        .configurationDisplayName("써니의 위젯")
-        .description("매분마다 새로운 이모지와 퇴근시간을 알 수 있습니다.")
+        .configurationDisplayName("써니의 컬러위젯")
+        .description("이모지와 색상을 골라보세요!")
         .supportedFamilies([.systemMedium, .systemSmall, .systemLarge])
     }
 }
